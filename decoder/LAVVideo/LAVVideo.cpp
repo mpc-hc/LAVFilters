dca995e8145e94620be2d097c7a55a2830575f47
Underground78 <underground78@users.sourceforge.net>
2013-09-28 17:30:14 +0200

    [MPC-HC] Tray icon: Add an optional custom callback for "show property page" events.

--- decoder/LAVVideo/LAVVideo.cpp
+++ decoder/LAVVideo/LAVVideo.cpp
@@ -55,6 +55,7 @@ void CALLBACK CLAVVideo::StaticInit(BOOL bLoading, const CLSID *clsid)
 CLAVVideo::CLAVVideo(LPUNKNOWN pUnk, HRESULT* phr)
   : CTransformFilter(NAME("LAV Video Decoder"), 0, __uuidof(CLAVVideo))
   , m_Decoder(this)
+  , m_fpPropPageCallback(NULL)
 {
   *phr = S_OK;
   m_pInput = new CVideoInputPin(TEXT("CVideoInputPin"), this, phr, L"Input");
@@ -114,6 +115,7 @@ HRESULT CLAVVideo::CreateTrayIcon()
   if (CBaseTrayIcon::ProcessBlackList())
     return S_FALSE;
   m_pTrayIcon = new CBaseTrayIcon(this, TEXT(LAV_VIDEO), IDI_ICON1);
+  m_pTrayIcon->SetCustomOpenPropPage(m_fpPropPageCallback);
   return S_OK;
 }
 
@@ -410,6 +412,7 @@ STDMETHODIMP CLAVVideo::NonDelegatingQueryInterface(REFIID riid, void** ppv)
     QI(ISpecifyPropertyPages2)
     QI(IPropertyBag)
     QI2(ILAVVideoSettings)
+    QI2(ILAVVideoSettingsMPCHCCustom)
     QI2(ILAVVideoStatus)
     __super::NonDelegatingQueryInterface(riid, ppv);
 }
@@ -2516,4 +2519,13 @@ STDMETHODIMP CLAVVideo::SetH264MVCDecodingOverride(BOOL bEnabled)
 STDMETHODIMP CLAVVideo::GetHWAccelActiveDevice(BSTR *pstrDeviceName)
 {
   return m_Decoder.GetHWAccelActiveDevice(pstrDeviceName);
+}
+
+// ILAVVideoSettingsMPCHCCustom
+STDMETHODIMP CLAVVideo::SetPropertyPageCallback(HRESULT (*fpPropPageCallback)(IBaseFilter* pFilter))
+{
+  m_fpPropPageCallback = fpPropPageCallback;
+  if (m_pTrayIcon)
+    m_pTrayIcon->SetCustomOpenPropPage(fpPropPageCallback);
+  return S_OK;  
 }
